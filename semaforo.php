b9e2ee5708db9d426154664bac1ed7e014ba8bc2
uo293577 <158502509+Ivigaga@users.noreply.github.com>
2024-12-05 17:40:33 +0100

    Add files via upload

---
+++ semaforo.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+
+<html lang="es">
+<head>
+    <meta name="author" content="Iván García García" />
+    <meta name="description" content="Juego de Memoria" />
+    <meta name="keywords" content="F1,juegos, semáforo" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Datos que describen el documento -->
+    <link rel="icon" href="multimedia/imagenes/icono.ico" sizes="48x48" 
+        type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css" />
+    <link rel="stylesheet" type="text/css" href="estilo/layout.css" />
+    <link rel="stylesheet" type="text/css" href="estilo/semaforo.css" />
+    <meta charset="UTF-8" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="js/semaforo.js"></script>
+    <title>F1 Desktop: Semáforo</title>
+</head>
+
+<body>
+    <header>
+        <!-- Datos con el contenidos que aparece en el navegador -->
+        <h1><a title="Inicio" href="index.html">F1 Desktop</a></h1>
+        <nav>
+            <a title="Inicio" href="index.html">Inicio</a>
+            <a title="Piloto" href="piloto.html">Piloto</a>
+            <a title="Notocias" href="noticias.html">Noticias</a>
+            <a title="Calendario" href="calendario.html">Calendario</a>
+            <a title="Meteorología" href="meteorologia.html">Meteorología</a>
+            <a title="Circuito" href="circuito.html">Circuito</a>
+            <a title="Viajes" href="viajes.php">Viajes</a>
+            <a title="Juegos" class="active" href="juegos.html">Juegos</a>
+        </nav>
+    </header>
+    <p>Estás en <a title="Inicio" href="index.html">Inicio</a> | <a title="Juegos" href="juegos.html">Juegos</a>  | Semáforo</p>
+    <nav>
+        <a title="Memoria" href="memoria.html">Memoria</a>
+        <a title="Semáforo" href="semaforo.html">Semáforo</a>
+    </nav>
+    <main>
+    <script>
+        var semaforo=new Semaforo();
+    </script>
+    
+    </main>
+<?php
+class Record {
+    public function __construct() {
+        $this->server = "localhost";
+        $this->user = "DBUSER2024";
+        $this->pass = "DBPSWD2024";
+        $this->dbname = "records";
+    }
+}
+
+if (count($_POST) > 0) {
+    $record = new Record();
+    $db = new mysqli($record->server, $record->user, $record->pass, $record->dbname);
+
+    // Comprobar si ya existe el registro
+    $consultaCheck = $db->prepare("SELECT * FROM registro WHERE nombre = ? AND apellidos = ? ");
+    $consultaCheck->bind_param('ss', $_POST["nombre"], $_POST["apellido"]);
+    $consultaCheck->execute();
+    $resultado = $consultaCheck->get_result();
+    $already_in=false;
+    if ($resultado->num_rows >= 1) {
+        
+        while ($row = $resultado->fetch_array()) {
+           if($row['nivel']==$_POST["dificultad"] && $row['tiempo']==$_POST["tiempo"]){
+                $already_in=true;
+                break;
+           }
+        }
+    }
+    $consultaCheck->close();
+    if ($already_in==false) {
+        // Si no existe, insertar el registro
+        $consultaInsert = $db->prepare("INSERT INTO registro (nombre, apellidos, nivel, tiempo) VALUES (?,?,?,?)");
+        $consultaInsert->bind_param('ssdd', $_POST["nombre"], $_POST["apellido"], $_POST["dificultad"], $_POST["tiempo"]);
+        $consultaInsert->execute();
+        $consultaInsert->close();
+    }
+
+    // Consultar el ranking
+    $consultaRanking = $db->prepare("SELECT * FROM registro WHERE nivel = ? ORDER BY tiempo asc LIMIT 10");
+    $consultaRanking->bind_param('d', $_POST["dificultad"]);
+    $consultaRanking->execute();
+    $resultadoRanking = $consultaRanking->get_result();
+
+    if ($resultadoRanking->num_rows >= 1) {
+        echo("<h3>Ranking</h3>");
+        echo("<ol>");
+        while ($row = $resultadoRanking->fetch_array()) {
+            echo("<li> Nombre: " . $row['nombre'] . " Apellidos: " . $row['apellidos'] . " Dificultad: " . $row['nivel'] . " Tiempo: " . $row['tiempo'] . "s </li>");
+        }
+        echo("</ol>");
+    }
+    $consultaRanking->close(); // Cerrar la consulta
+    $db->close();
+}
+?>
+
+</body>
+</html>
--- viajes.php
+++ viajes.php
@@ -1,7 +1,45 @@
 <!DOCTYPE HTML>
 
 <html lang="es">
+<?php
+class Carrusel{
+    public function __construct($pais,$capital){
+        $this->pais=$pais;
+        $this->capital=$capital;
+    }
 
+    public function generatePhotos(){
+        $url="https://www.flickr.com/services/feeds/photos_public.gne?";
+        $url.='&tags='. $this->pais;
+        $url.= '&per_page=10';
+        $url.= '&format=json';
+        $url.= '&nojsoncallback=1';
+        $respuesta = file_get_contents($url);
+        return json_decode($respuesta);
+    }
+}
+
+class Moneda{
+    public function __construct($local,$cambio){
+        $this->local=$local;
+        $this->cambio=$cambio;
+    }
+
+    public function obtenerCambioDeMoneda(){
+        $key="cb32e2e3f50dd6ae22f0b25c";
+        $url="https://v6.exchangerate-api.com/v6/";
+        $url.=$key;
+        $url.="/pair";
+        $url.="/".$this->cambio;
+        $url.="/".$this->local;
+        $response_json = file_get_contents($url);
+        $response = json_decode($response_json);
+        print "<h3> Cambio de Moneda </h3>";
+        print "<p> 1 ".$this->cambio ." --> " . $response->conversion_rate. " " .$this->local." </p>";
+    }
+}
+    
+?>
 <head>
     <meta name="author" content="Iván García García" />
     <meta name="description" content="Viajes" />
@@ -11,6 +49,7 @@
     <link rel="icon" href="multimedia/imagenes/icono.ico" sizes="48x48" type="image/vnd.microsoft.icon">
     <link rel="stylesheet" type="text/css" href="estilo/estilo.css" />
     <link rel="stylesheet" type="text/css" href="estilo/layout.css" />
+    <link rel="stylesheet" type="text/css" href="estilo/carrusel.css" />
     <meta charset="UTF-8" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
@@ -31,18 +70,41 @@
             <a title="Calendario" href="calendario.html">Calendario</a>
             <a title="Meteorología" href="meteorologia.html">Meteorología</a>
             <a title="Circuito" href="circuito.html">Circuito</a>
-            <a title="Viajes" class="active" href="viajes.html">Viajes</a>
+            <a title="Viajes" class="active" href="viajes.php">Viajes</a>
             <a title="Juegos" href="juegos.html">Juegos</a>
         </nav>
     </header>
     <p>Estás en <a title="Inicio" href="index.html">Inicio</a> | Viajes </p>
     <h2>Viajes</h2>
+    <?php
+        $moneda=new Moneda('EUR','USD');
+        $moneda->obtenerCambioDeMoneda();
+    ?>
+    <h3>Mapas</h3>
     <button onclick="viajes.getMapaEstaticoGoogle()">Mostrar mapa estático</button>
     <button onclick="viajes.initMap()">Mostrar mapa interactivo</button>
     <div id="mapa" ></div>
+    <h3>Carrusel de Imágenes</h3>
+    <article>
+        <?php
+        $carrusel=new Carrusel("Spain","Madrid");
+        $fotos=$carrusel->generatePhotos();
+        print "<article>";
+        for($i=0;$i<10;$i++){
+            $imgSrc = $fotos->items[$i]->media->m;
+            print "<img alt='Imagen de España' src=\"$imgSrc\" />";
+        } 
+        print "</article>";
+        ?>
+        <button> &gt; </button>
+        <button> &lt; </button>
+    </article>
+    
     <script>
         // Inicializamos la clase Viajes
-        const viajes = new Viajes();
+        var viajes = new Viajes();
+        viajes.createCarrusel();
     </script>
+    
 </body>
 </html>
