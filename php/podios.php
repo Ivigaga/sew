cbd56f9b5ff3e16d3478985a8803d102b062f106
uo293577 <158502509+Ivigaga@users.noreply.github.com>
2024-12-15 15:55:03 +0100

    Add files via upload

--- php/podios.php
+++ php/podios.php
@@ -311,8 +311,8 @@ try {
     <!-- Formulario para importar CSV -->
     <h3>Importar CSV</h3>
     <form action="#" method="POST">
-        <label for="csv_import">Selecciona un archivo CSV para importar:
-        <input type="file" name="csv_import" accept=".csv" required></label>
+        <p>Selecciona un archivo CSV para importar:</p>
+        <input type="file" name="csv_import"  accept=".csv" aria-label="Importar csv" required>
         <button type="submit">Importar CSV</button>
     </form>
 
